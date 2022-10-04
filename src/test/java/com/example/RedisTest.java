ce497a844d4c019f04735700d41068147cdbf851
정민지 <mjjeong3581@naver.com>
2022-10-04 13:56:14 +0900

    redis 설정

---
+++ src/test/java/com/example/RedisTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith({SpringExtension.class})
+class RedisTest {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void redisGetStringTest() {
+        Object result = redisTemplate.opsForValue().get("test");
+
+        assertThat(result).isEqualTo(null);
+    }
+
+    @Test
+    void redisSetStringTest() {
+        String userId = "userId";
+        redisTemplate.opsForValue().set(userId, "1");
+        Object result = redisTemplate.opsForValue().get(userId);
+
+        assertThat(result).isNotNull();
+    }
+}
